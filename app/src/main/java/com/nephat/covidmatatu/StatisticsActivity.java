fafeb494e3db6aca342505cabed533bac2527db6
Nephat Mutugi <muchirinephat5@gmail.com>
2020-05-22 00:48:11 +0300

    Statistics Activity and Recycler View

--- app/src/main/java/com/nephat/covidmatatu/StatisticsActivity.java
+++ app/src/main/java/com/nephat/covidmatatu/StatisticsActivity.java
@@ -4,12 +4,15 @@ import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.data.PieEntry;
 import com.github.mikephil.charting.utils.ColorTemplate;
+import com.nephat.covidmatatu.adapters.StatisticsRecyclerAdapter;
 
 import java.util.ArrayList;
 
@@ -17,6 +20,11 @@ public class StatisticsActivity extends AppCompatActivity {
 
     private static final String TAG = "StatisticsActivity";
 
+    //Vars
+    private ArrayList<String> countyNames = new ArrayList<>();
+    private ArrayList<String> countyCases = new ArrayList<>();
+
+    //Widgets
     private PieChart pieChart;
 
     @Override
@@ -27,8 +35,8 @@ public class StatisticsActivity extends AppCompatActivity {
         pieChart = findViewById(R.id.pieChart);
 
         ArrayList<PieEntry> patients = new ArrayList<>();
-        patients.add(new PieEntry(555, "Active cases"));
-        patients.add(new PieEntry(358, "Total recovered"));
+        patients.add(new PieEntry(613, "Active cases"));
+        patients.add(new PieEntry(366, "Total recovered"));
         patients.add(new PieEntry(50, "Total Deaths"));
 
         PieDataSet pieDataSet = new PieDataSet(patients, "Coronavirus cases");
@@ -43,5 +51,86 @@ public class StatisticsActivity extends AppCompatActivity {
         pieChart.setCenterText("Coronavirus cases");
         pieChart.animate();
 
+        getData();
+
+    }
+
+    private void getData(){
+        countyNames.add("Nairobi County");
+        countyCases.add("400");
+
+        countyNames.add("Mombasa County");
+        countyCases.add("300");
+
+        countyNames.add("Mandera County");
+        countyCases.add("70");
+
+        countyNames.add("Kajiado County");
+        countyCases.add("55");
+
+        countyNames.add("Kisumu County");
+        countyCases.add("50");
+
+        countyNames.add("Homabay County");
+        countyCases.add("46");
+
+        countyNames.add("Siaya County");
+        countyCases.add("40");
+
+        countyNames.add("Machakos County");
+        countyCases.add("30");
+
+        countyNames.add("Kilifi County");
+        countyCases.add("20");
+
+        countyNames.add("Kiambu County");
+        countyCases.add("12");
+
+        countyNames.add("Kwale County");
+        countyCases.add("12");
+
+        countyNames.add("Busia County");
+        countyCases.add("10");
+
+        countyNames.add("Muran'ga County");
+        countyCases.add("8");
+
+        countyNames.add("Uasin Gishu County");
+        countyCases.add("8");
+
+        countyNames.add("Kericho County");
+        countyCases.add("5");
+
+        countyNames.add("Bungoma County");
+        countyCases.add("5");
+
+        countyNames.add("Wajir County");
+        countyCases.add("5");
+
+        countyNames.add("Lamu County");
+        countyCases.add("3");
+
+        countyNames.add("Garissa County");
+        countyCases.add("2");
+
+        countyNames.add("Embu County");
+        countyCases.add("1");
+
+        countyNames.add("Meru County");
+        countyCases.add("1");
+
+        countyNames.add("Kirinyaga County");
+        countyCases.add("1");
+
+
+        init();
+    }
+
+    private void init(){
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        RecyclerView recyclerView = findViewById(R.id.statistics_recyclerView);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        StatisticsRecyclerAdapter adapter = new StatisticsRecyclerAdapter(this, countyNames, countyCases);
+        recyclerView.setAdapter(adapter);
     }
 }
--- app/src/main/java/com/nephat/covidmatatu/adapters/StatisticsRecyclerAdapter.java
+++ app/src/main/java/com/nephat/covidmatatu/adapters/StatisticsRecyclerAdapter.java
@@ -1,6 +1,10 @@
 package com.nephat.covidmatatu.adapters;
 
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -8,17 +12,52 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.nephat.covidmatatu.R;
 
-public class StatisticsRecyclerAdapter {
+import java.util.ArrayList;
+
+public class StatisticsRecyclerAdapter extends RecyclerView.Adapter<StatisticsRecyclerAdapter.ViewHolder> {
+
+    private static final String TAG = "StatisticsRecyclerAdapt";
+
+    private ArrayList<String> countyNames = new ArrayList<>();
+    private ArrayList<String> countyCases = new ArrayList<>();
+    private Context mContext;
+
+    public StatisticsRecyclerAdapter(Context mContext, ArrayList<String> countyNames,
+                                     ArrayList<String> countyCases) {
+        this.countyNames = countyNames;
+        this.countyCases = countyCases;
+        this.mContext = mContext;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Log.d(TAG, "onCreateViewHolder: Called");
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.counties_layout_list_items, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.countyName.setText(countyNames.get(position));
+        holder.countyNoOfCases.setText(countyCases.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return countyNames.size();
+    }
 
     class ViewHolder extends RecyclerView.ViewHolder{
 
         TextView countyName;
-        TextView countyCases;
+        TextView countyNoOfCases;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             countyName = itemView.findViewById(R.id.textCountyName);
-            countyCases = itemView.findViewById(R.id.textCasesNumber);
+            countyNoOfCases = itemView.findViewById(R.id.textCasesNumber);
         }
     }
 }
